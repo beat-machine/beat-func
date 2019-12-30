34fb565c36ef2e648f0cbe31b7af7e833643b723
dhsavell <dhsavell05@gmail.com>
2019-12-30 17:09:56 -0500

    Update for release

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from tempfile import NamedTemporaryFile, mkstemp
 
 import beatmachine as bm
 import ffmpeg
-from fastapi import FastAPI, Form, File, UploadFile
+from fastapi import FastAPI, Form, File, UploadFile, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import StreamingResponse
+from starlette.responses import StreamingResponse, JSONResponse
 from typing import List
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 from pydantic import BaseModel
 
 logger = logging.getLogger("beatfunc")
@@ -23,6 +24,7 @@ app = FastAPI()
 origins = [
     "https://mystifying-heisenberg-1d575a.netlify.com",
     "https://beatmachine.branchpanic.me",
+    "https://tbm.branchpanic.me",
 ]
 
 if os.getenv("BEATFUNC_ALL_ORIGINS"):
@@ -33,9 +35,21 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def _settings_to_kwargs(settings: dict) -> dict:
+    kwargs = {"min_bpm": 60, "max_bpm": 300}
+
+    if "suggested_bpm" in settings:
+        suggested_bpm = settings["suggested_bpm"]
+        drift = settings.pop("drift", 15)
+        kwargs["min_bpm"] = suggested_bpm - drift
+        kwargs["max_bpm"] = suggested_bpm + drift
+
+    return kwargs
 
 
 async def process_song(
@@ -55,6 +69,7 @@ async def process_song(
 
     buf = io.BytesIO()
     beats.consolidate().export(buf, format="mp3")
+    os.remove(filename)
     elapsed = timeit.default_timer() - start_time
     logger.info(f"Finished in {elapsed}s, streaming result to client")
     buf.seek(0)
@@ -74,19 +89,46 @@ async def process_song_from_youtube(payload: YoutubeSongPayload):
         effects = [bm.effects.load_from_dict(e) for e in payload.effects]
     except TypeError as e:
         logger.error(f"Invalid effect data: {e}")
-        return "Invalid effect data", http.HTTPStatus.BAD_REQUEST
+        raise HTTPException(
+            detail="Invalid effect data", status_code=http.HTTPStatus.BAD_REQUEST
+        )
 
     logger.info("Downloading file")
-    raw_filename = (
-        YouTube(payload.youtube_url).streams.filter(only_audio=True).first().download()
-    )
-    mp3_filename = str(uuid.uuid4())
+
+    try:
+        yt = YouTube(payload.youtube_url)
+
+        if (
+            yt.player_config_args.get("player_response")
+            .get("videoDetails")
+            .get("lengthSeconds")
+            > 360
+        ):
+            raise HTTPException(
+                detail="Video is too long (max is 6 minutes)",
+                status_code=http.HTTPStatus.REQUEST_ENTITY_TOO_LARGE,
+            )
+
+        raw_filename = (
+            YouTube(payload.youtube_url)
+            .streams.filter(only_audio=True)
+            .first()
+            .download(filename=str(uuid.uuid4()))
+        )
+    except KeyError:
+        raise HTTPException(
+            detail="Failed to download video", status_code=http.HTTPStatus.BAD_REQUEST
+        )
+
+    mp3_filename = str(uuid.uuid4()) + ".mp3"
 
     logger.info("Converting to mp3")
     ffmpeg.input(raw_filename).output(mp3_filename).run()
     os.remove(raw_filename)
 
-    return await process_song(effects, mp3_filename, payload.settings)
+    return await process_song(
+        effects, mp3_filename, _settings_to_kwargs(payload.settings)
+    )
 
 
 @app.post("/")
@@ -99,36 +141,40 @@ async def process_song_from_file(
         settings = {}
 
         if isinstance(effect_data, dict):
-            settings = effect_data["settings"]
+            settings = effect_data.pop("settings", {})
             effect_data = effect_data["effects"]
 
         try:
             effects = [bm.effects.load_from_dict(e) for e in effect_data]
         except TypeError as e:
             logger.error(f"Invalid effect data: {e}")
-            return "Invalid effect data", http.HTTPStatus.BAD_REQUEST
+            raise HTTPException(
+                detail="Invalid effect data", status_code=http.HTTPStatus.BAD_REQUEST
+            )
     except KeyError as e:
         logger.error(f"KeyError when parsing JSON, assuming missing data: {e}")
-        return "Missing effects", http.HTTPStatus.BAD_REQUEST
+        raise HTTPException(
+            detail="Missing effects", status_code=http.HTTPStatus.BAD_REQUEST
+        )
     except ValueError as e:
         logger.error(f"ValueError when parsing JSON, assuming malformed data: {e}")
-        return "Invalid effects", http.HTTPStatus.BAD_REQUEST
+        raise HTTPException(
+            detail="Invalid effects", status_code=http.HTTPStatus.BAD_REQUEST
+        )
 
     if len(effects) > 5:
-        return "Too many effects (max is 5)", http.HTTPStatus.BAD_REQUEST
+        raise HTTPException(
+            detail="Too many effects (max is 5)",
+            status_code=http.HTTPStatus.BAD_REQUEST,
+        )
     if len(effects) < 1:
-        return "Not enough effects (min is 1)", http.HTTPStatus.BAD_REQUEST
+        raise HTTPException(
+            detail="Not enough effects (min is 1)",
+            status_code=http.HTTPStatus.BAD_REQUEST,
+        )
 
     with NamedTemporaryFile(suffix=".mp3", delete=False) as fp:
         fp.write(await song.read())
         filename = fp.name
 
-    kwargs = {"min_bpm": 60, "max_bpm": 300}
-
-    if "suggested_bpm" in settings:
-        suggested_bpm = settings["suggested_bpm"]
-        drift = settings.pop("drift", 15)
-        kwargs["min_bpm"] = suggested_bpm - drift
-        kwargs["max_bpm"] = suggested_bpm + drift
-
-    return await process_song(effects, filename, kwargs)
+    return await process_song(effects, filename, _settings_to_kwargs(settings))
