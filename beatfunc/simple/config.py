cfdda67ecc9f847229df6b82a639c25a02018016
branchpanic <3802908+branchpanic@users.noreply.github.com>
2023-04-14 15:50:54 -0700

    Fix missing config namespace in beatfunc.simple

--- beatfunc/simple/config.py
+++ beatfunc/simple/config.py
@@ -4,7 +4,7 @@ from .. import core
 
 _ns = core.config.conf_namespace("cloud_run")
 
-ORIGINS = (os.getenv("origins", "*")).split(";")
-ALLOW_URLS = bool(int(os.getenv("allow_yt", "1")))
+ORIGINS = (os.getenv(_ns("origins"), "*")).split(";")
+ALLOW_URLS = bool(int(os.getenv(_ns("allow_urls"), "1")))
 
 del _ns
